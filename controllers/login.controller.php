f4c524c1f5d3d74bd5c7de63eac4e8895cd030f6
aldrei <aldreicr@gmail.com>
2025-01-25 21:40:34 -0300

    added Usuario class as placeholder

--- controllers/login.controller.php
+++ controllers/login.controller.php
@@ -2,6 +2,7 @@
 if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     $resultado = $database->query(
         "insert into usuarios ( nome, email, senha ) values ( :nome, :email, :senha )",
+        Usuario::class,
         [
             'nome' => $_POST['nome'],
             'email' => $_POST['email'],
