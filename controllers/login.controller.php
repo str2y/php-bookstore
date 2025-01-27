74990ee9f41a64020d5c8d2ce76560db04a627fe
aldrei <aldreicr@gmail.com>
2025-01-26 22:01:53 -0300

    removed placeholder class Usuario

--- controllers/login.controller.php
+++ controllers/login.controller.php
@@ -2,7 +2,7 @@
 if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     $resultado = $database->query(
         "insert into usuarios ( nome, email, senha ) values ( :nome, :email, :senha )",
-        Usuario::class,
+        Livro::class,
         [
             'nome' => $_POST['nome'],
             'email' => $_POST['email'],
--- models/Usuario.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-class Usuario{
-    public $id;
-    public $nome;
-    public $email;
-    public $senha;
-}
